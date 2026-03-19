8f491cb7d33157b957ccfb1a8bbdec33c5d19093
Ricardo Nunez <ricardonunez@MacBook-Air-de-Ricardo.local>
2026-03-19 16:53:58 -0500

    modify backend and pipeline

--- backend.tf
+++ backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "test-cncf-ric-19032026"
-    key            = "prod/ec2/terraform.tfstate" 
-    region         = "us-east-1"
-  }
+  backend "s3" {}
 }
