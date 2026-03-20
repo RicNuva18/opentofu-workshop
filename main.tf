6717bbfc3c51ee44d363009030c9f6e820644807
Ricardo Nunez <ricardonunez@MacBook-Air-de-Ricardo.local>
2026-03-19 21:02:52 -0500

    test good

--- main.tf
+++ main.tf
@@ -10,13 +10,6 @@ resource "aws_subnet" "private" {
   tags                    = var.mandatory_tags
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true 
-  tags                    = var.mandatory_tags
-}
-
 resource "aws_security_group" "safe_sg" {
   name   = "safe-sg"
   vpc_id = aws_vpc.main.id
@@ -30,19 +23,6 @@ resource "aws_security_group" "safe_sg" {
   tags = var.mandatory_tags
 }
 
-resource "aws_security_group" "unsafe_sg" {
-  name   = "unsafe-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = var.mandatory_tags
-}
-
 resource "aws_instance" "test_compliant" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
@@ -50,36 +30,4 @@ resource "aws_instance" "test_compliant" {
   vpc_security_group_ids      = [aws_security_group.safe_sg.id]
   associate_public_ip_address = false
   tags                        = var.mandatory_tags
-}
-
-resource "aws_instance" "test_fails_port" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private.id
-  vpc_security_group_ids      = [aws_security_group.unsafe_sg.id]
-  associate_public_ip_address = false
-  tags                        = var.mandatory_tags
-}
-
-resource "aws_instance" "test_fails_subnet" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.safe_sg.id]
-  associate_public_ip_address = true
-  tags                        = var.mandatory_tags
-}
-
-resource "aws_instance" "test_fails_tags" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private.id
-  vpc_security_group_ids      = [aws_security_group.safe_sg.id]
-  associate_public_ip_address = false
-  
-  tags = {
-    owner       = "equipo-devops"
-    environment = "produccion"
-    app         = "backend-api"
-  }
 }
